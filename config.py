83fe318278d2154f6bd196e2473dbcac705ac92c
Bestin Antu <57059472+bestin-07@users.noreply.github.com>
2025-01-30 10:59:00 +0100

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-ALPHA_VANTAGE_API_KEY = 'your_alpha_vantage_api_key_here'
-EMAIL_ADDRESS = 'your_email@example.com'
-EMAIL_PASSWORD = 'your_email_password_here'
-SMTP_SERVER = 'smtp.example.com'
-SMTP_PORT = 587
